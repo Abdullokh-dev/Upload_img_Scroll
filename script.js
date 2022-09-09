6d416e103a1a64a067e4a4b33e1e0fab37c4bb78
Abdullokh_dev <101015366+Abdullokh-dev@users.noreply.github.com>
2022-09-09 10:30:40 +0500

    Dispaying data

--- script.js
+++ script.js
@@ -1,16 +1,48 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
 
 // Unsplash API
 const count = 10;
-const apiKey = 'YOUR_API_KEY';
+const apiKey = 'API_KEY';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+// Creat elements for links & photos, add to dom
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.hmtl,
+            target: '_blank'
+        })
+
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        });
+
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
 
 // Get Photos from Unsplash API
 
 async function getPhotos() {
     try{
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos()
     } catch (error) {
 
     }
